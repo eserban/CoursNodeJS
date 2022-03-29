c5aac70ccda2d39d368a755e236361952b546b49
Eduard Serban <serbanedi.fr@gmail.com>
2022-03-29 11:12:13 +0200

    forth commit

--- index.mjs
+++ index.mjs
@@ -3,21 +3,26 @@ import { getAll, getByName } from './products.mjs';
 
 http.createServer( (req, res) => {
 
+  
+
   if (req.url === "/products" && req.method === "GET") {
         //set the response
+        res.writeHead(200, {"Content-Type": "application/json"});
         res.write(JSON.stringify(getAll()));
         //end the response
         res.end();
-    }
-
-    if(req.url.startsWith('/products/') && req.method === "GET") {
-      const name = req.url.split('/')[2];
-      const pdt = getByName(name);
+      }
+      
+      if(req.url.startsWith('/products/') && req.method === "GET") {
+        const name = req.url.split('/')[2];
+        const pdt = getByName(name);
+        res.writeHead(200, {"Content-Type": "application/json"});
         res.write(pdt ? JSON.stringify(pdt) : "Cannot find product with name " + name);
         res.end();
-    }
-
-    else {
+      }
+      
+      else {
+      res.writeHead(404);
       res.write(`Cannot ${req.method.toLowerCase()} ${req.url}`);
       res.end();
     }
