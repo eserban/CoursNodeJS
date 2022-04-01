80245d50550596c0ffa068fb9e9a158dfbf6f6da
Eduard Serban <serbanedi.fr@gmail.com>
2022-04-01 11:54:37 +0200

    last commit before merge

--- test/calc.mjs
+++ test/calc.mjs
@@ -1,12 +1,29 @@
+/**
+ * Add 2 numbers
+ * @param {Int} a 
+ * @param {Int} b 
+ * @returns a + b
+ */
 const Add = (a, b) => {
     return a + b;
 }
 
-
+/**
+ * Substract 2 numbers
+ * @param {Int} a 
+ * @param {Int} b 
+ * @returns 
+ */
 const Substract = (a, b) => {
     return a - b;
 }
 
+/**
+ * Multiply 2 numbers
+ * @param {Int} a 
+ * @param {Int} b 
+ * @returns multiplication
+ */
 const Multiply = (a, b) => {
     return a*b;
 }
